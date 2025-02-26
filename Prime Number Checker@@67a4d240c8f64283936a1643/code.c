e9a5eae3e4b079cf61880d880729ff86500424ea
Raghav Gupta <raghav1634.becse24@chitkara.edu.in>
2025-02-26 14:44:07 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -16,13 +16,3 @@ void isPrime(int num){
     }
     return ;
 }
-int main() {
-    int t;
-    scanf("%d", &t);
-    while(t--){
-        int num;
-        scanf("%d", &num);
-        isPrime(num);
-    }
-    return 0;
-}
