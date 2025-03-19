44f7610aa208bcc5afed2325a5bdc2cfcec1445b
Raghav Gupta <raghav1634.becse24@chitkara.edu.in>
2025-03-19 20:05:27 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,13 @@
+// Your code here...
+int fibonacci(int n){
+    int a=0;
+    int b=1;
+    int c;
+    for(int i =1;i<=n;i++){
+        printf("%d",a);
+        c=a+b;
+        b=c;
+        a=b;
+    }
+    return;
+}
